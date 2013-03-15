4a85b40a25167b288ac7bd5c8be110cfb769a6a6
Kenneth Shinozuka <kennethshinozuka@gmail.com>
2013-03-14 20:30:21 -0400

    ha
    ha

---
+++ Project Mango Gamma Release/UINavigationItem+JTRevealSidebar.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationItem+JTRevealSidebar.h
+//  Project Mango Gamma Release
+//
+//  Created by Maria Feng on 3/14/13.
+//  Copyright (c) 2013 Sahil Gupta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UINavigationItem_JTRevealSidebar : NSObject
+
+@end
